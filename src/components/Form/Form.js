d6b11f9f2666b017ac4a1869acc389cdd5f733ac
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-06-02 11:48:43 -0400

    Form btn at Contact component done

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,8 +4,12 @@ import './Form.css';
 class Form extends Component {
   render() {
     return (
-      <div className="Form">
-        <h1>Form</h1>
+      <div className="Message-me">
+        <div className="Phone">
+          <a href="tel://1-939-232-3881">
+            +1 (939) 232-3881
+          </a>
+        </div>
       </div>
     );
   }
