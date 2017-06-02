e348850176e56c47cf1c8d7e99a8a4a173d5674e
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-06-02 14:27:14 -0400

    Fixed header and footer, scrollable body

--- src/containers/Body/Body.js
+++ src/containers/Body/Body.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './Body.css'
+import Home from '../../components/Home/Home.js';
+import About from '../../components/About/About.js';
+import Projects from '../../components/Projects/Projects.js';
 import Contact from '../../components/Contact/Contact.js';
 
 class Body extends Component {
   render() {
     return (
       <div className="Body">
+        <Home />
+        <About />
+        <Projects />
         <Contact />
       </div>
     );
