8b815e073e447ead04d227f9a50cdc3b3f9ae4b1
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-06-06 17:31:29 -0400

    Mobile nav menu working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react';
 import Nav from '../../containers/Nav/Nav.js';
-import Menu from '../Menu/Menu.js';
 import './Header.css';
 
 class Header extends Component {
-
-  constructor () {
-    super();
-
-    this.state = {
-      showMenu: false
-    };
-
-    this._getMenu = this._getMenu.bind(this);
-    this._handleClick = this._handleClick.bind(this);
-    this._getNav = this._getNav.bind(this);
-  }
-
   render() {
-
-  let nav = this._getNav();
-  let menu = this._getMenu();
-  let MenuBtn = nav;
-
-  if (this.state.showMenu) {
-    MenuBtn = menu;
-  }
-  
     return (
       <div className="Header">
         <div className="Header-nav">
-          <button className="Menu-btn" onClick={ this._handleClick }>
-            { MenuBtn }
+          <button className="Menu-btn" onClick={ this.props._toggleMenu }>
+            <Nav />
           </button>
         </div>
         <div className="Btns">
@@ -49,24 +26,6 @@ class Header extends Component {
           </button>
         </div>
       </div>
-    );
-  }
-  
-  _getNav() {
-    return (
-      <Nav />
-    );
-  }
-
-  _getMenu() {
-    return (
-      <Menu />
-    );
-  }
-
-  _handleClick() {
-    return (
-      this.setState({ showMenu: true })
     );
   }
 }
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import Body from '../../containers/Body/Body';
 import Footer from '../../components/Footer/Footer';
+import Menu from '../../components/Menu/Menu';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false
+    }
+    
+    this._toggleMenu = this._toggleMenu.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Body />
+        <Header _toggleMenu={ this._toggleMenu } />
+        { this.state.showMenu ? <Menu /> : <Body /> }
         <Footer />
       </div>
     );
   }
+  
+  _toggleMenu() {
+      this.setState({ showMenu: !this.state.showMenu })
+  }
+
 }
 
 export default App;
